dd61336946c371c6a9556d22dab49b7204301193
soykim <soykim76@gmail.com>
2020-03-26 17:01:53 +0900

    class room...

---
+++ movie1.R
@@ -0,0 +1,32 @@
+# [ 실습1 ]
+# 다음영화 사이트에 올려진 (http://movie.daum.net/) 댓글에 대하여 
+# 고객 평점과 리뷰글을 1페이지(10개)만 스크래핑하여 
+# 데이터프레임 형식으로 만들어 "daummovie1.csv" 로 저장한다.
+# 
+# https://movie.daum.net/moviedb/grade?movieId=127122 
+# 또는
+# https://movie.daum.net/moviedb/grade?movieId=131576
+# R 코드는 movie1.R 로 생성하여 csv 파일과 함께 제출하세요.
+
+
+#쥬디
+url <- "https://movie.daum.net/moviedb/grade?movieId=131576"
+data <- read_html(url)
+score <- html_nodes(data, ".emph_grade")
+score <- html_text(score)
+
+review <- html_nodes(data, ".desc_review")
+review <- html_text(review, trim = T)
+
+write.csv(data.frame(score, review), "data/daummovie1.csv")
+
+#작은 아씨들
+url <- "https://movie.daum.net/moviedb/grade?movieId=127122"
+data <- read_html(url)
+score <- html_nodes(data, ".emph_grade")
+score <- html_text(score)
+
+review <- html_nodes(data, ".desc_review")
+review <- html_text(review, trim = T)
+
+write.csv(data.frame(score, review), "data/daummovie1_little.csv")
---
+++ movie2.R
@@ -0,0 +1,48 @@
+# [ 실습1 ]
+# 다음영화 사이트에 올려진 (http://movie.daum.net/) 댓글에 대하여 
+# 고객 평점과 리뷰글을 1페이지(10개)만 스크래핑하여 
+# 데이터프레임 형식으로 만들어 "daummovie1.csv" 로 저장한다.
+# 
+# https://movie.daum.net/moviedb/grade?movieId=127122 
+# 또는
+# https://movie.daum.net/moviedb/grade?movieId=131576
+# R 코드는 movie1.R 로 생성하여 csv 파일과 함께 제출하세요.
+
+# 이번에는 평점과 리뷰글을 20페이지까지 스크래핑하여 데이터프레임으로 만들어 "daummovie2.csv" 로 저장한다.
+# R 코드는 movie2.R 로 생성하여 csv 파일과 함께 제출하세요.
+
+
+#작은 아씨들
+url <- "https://movie.daum.net/moviedb/grade?movieId=127122&type=netizen&page="
+movie <- NULL
+
+for(i in 1:100){
+  url_page <- paste0(url, i)
+  data_page <- read_html(url_page)
+  score_page <- html_text(html_nodes(data_page, ".emph_grade"))
+  if(length(score_page)<1) break
+  review_page <- html_text(html_nodes(data_page, ".desc_review"), trim = T)
+  movie_page <- data.frame(score_page, review_page)
+  movie <- rbind(movie, movie_page)
+}
+
+write.csv(movie, "data/daummovie2.csv")
+
+
+#쥬디 --- while의 무한루프로 구현
+url <- "https://movie.daum.net/moviedb/grade?movieId=131576&type=netizen&page="
+movie <- NULL
+
+i <- 1
+while(T){
+  url_page <- paste0(url, i)
+  data_page <- read_html(url_page)
+  score_page <- html_text(html_nodes(data_page, ".emph_grade"))
+  if(length(score_page)<1) break
+  review_page <- html_text(html_nodes(data_page, ".desc_review"), trim = T)
+  movie_page <- data.frame(score_page, review_page)
+  movie <- rbind(movie, movie_page)
+  i <- i+1
+}
+
+write.csv(movie, "data/daummovie2_judy.csv")
